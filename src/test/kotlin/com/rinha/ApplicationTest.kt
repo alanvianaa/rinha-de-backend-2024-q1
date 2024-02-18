11151a7e257bfede3acab8a5db111623dbc117e9
Alan Viana <alanviana.nascimento@gmail.com>
2024-02-17 21:42:27 -0300

    fix: hello route test

--- src/test/kotlin/com/rinha/ApplicationTest.kt
+++ src/test/kotlin/com/rinha/ApplicationTest.kt
@@ -1,17 +1,17 @@
 package com.rinha
 
+import com.rinha.routes.helloRoute
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.routing.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            helloRouting()
-        }
+        application { routing { helloRoute() } }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
