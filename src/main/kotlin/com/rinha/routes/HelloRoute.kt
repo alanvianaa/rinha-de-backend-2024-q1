8e989812e4bc4b62b77e490b4bd5fba35017848e
Alan Viana <alanviana.nascimento@gmail.com>
2024-02-24 13:33:10 -0300

    chore: swagger

--- src/main/kotlin/com/rinha/routes/HelloRoute.kt
+++ src/main/kotlin/com/rinha/routes/HelloRoute.kt
@@ -1,6 +1,7 @@
 package com.rinha.routes
 
 import io.ktor.server.application.call
+import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
@@ -10,4 +11,5 @@ fun Route.helloRoute() {
     get("/") {
         call.respondText("Hello World!")
     }
+    swaggerUI(path = "swagger", swaggerFile = "documentation.yaml")
 }
